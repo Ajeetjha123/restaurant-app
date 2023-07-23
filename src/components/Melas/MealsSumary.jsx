5bc164c44edec267ca68c19e953e66cae638e100
Ajeet Jha <kumarjhaajeet653@gmail.com>
2023-07-23 23:05:19 +0530

    Meal summary added

---
+++ src/components/Melas/MealsSumary.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import classes from './MealsSumary.module.css'
+const MealsSumary = () => {
+  return <section className={classes.Summary}>
+    <h2>Delicious Food, Delevered To You</h2>
+    <p>Choose your favroite meals from our board selection of avalible meals and enjoy a delicious lunch or dinner at home.</p>
+    <p>All our meals are cooked with high-quality ingredients, just-in-time and of course by exprinced chiefs!</p>
+  </section>
+}
+
+export default MealsSumary
