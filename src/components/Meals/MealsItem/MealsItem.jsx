81186e7055361c71be7e6a05d266003f0a13cd25
Ajeet Jha <kumarjhaajeet653@gmail.com>
2023-07-24 10:50:03 +0530

    form created

--- src/components/Meals/MealsItem/MealsItem.jsx
+++ src/components/Meals/MealsItem/MealsItem.jsx
@@ -1,5 +1,6 @@
-import React from 'react'
-import classes from './MealsItem.module.css'
+import React from 'react';
+import classes from './MealsItem.module.css';
+import MealItemForm from './MealItemForm';
 const MealsItem = (props) => {
   const price = `$${props.price.toFixed(2)}`
   return <li className={classes.meal}>
@@ -8,7 +9,9 @@ const MealsItem = (props) => {
       <div className={classes.description}>{props.description}</div>
       <div className={classes.price}>{price}</div>
     </div>
-    <div></div>
+    <div>
+      <MealItemForm />
+    </div>
   </li>
 }
 export default MealsItem
